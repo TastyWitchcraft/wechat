c1ed5a8439818ba85c0fdd5d991019e342b58c41
成都富顿科技－张庆 <zhangqing@cdfortis.com>
2019-06-20 15:17:02 +0800

    界面的接口调试、界面调整

--- service/src/main/webapp/static/js/global.js
+++ service/src/main/webapp/static/js/global.js
@@ -1,4 +1,6 @@
 
 let userId = "";
-// const baseUrl = 'http://tastylove.xicp.net:25410/';
-const baseUrl = 'http://localhost:8080/';
+const baseUrl = 'http://tastylove.xicp.net:25410/';
+// const baseUrl = 'http://localhost:8080/';
+//中心数据传输
+const vueBus = new Vue();
