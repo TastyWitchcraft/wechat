3e1edd47c87b8de835136ef19b34b6d1e6b494c1
TastyWitchcraft <836862871@qq.com>
2019-06-22 20:42:03 +0800

    优化

--- service-boot/src/main/java/com/tasty/cowechat/application/CowechatApplication.java
+++ service-boot/src/main/java/com/tasty/cowechat/application/CowechatApplication.java
@@ -3,15 +3,21 @@ package com.tasty.cowechat.application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.tasty")
 @MapperScan("com.tasty.mybatis.mapper")
-public class CowechatApplication {
+public class CowechatApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CowechatApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CowechatApplication.class);
+    }
 }
